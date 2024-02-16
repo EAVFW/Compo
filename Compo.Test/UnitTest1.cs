afccebad97956e8ce8b26da81eb3842a20da5445
Thyge Skødt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2024-02-16 08:54:56 +0100

    test(function): Add 'add' tests

--- Compo.Test/UnitTest1.cs
+++ Compo.Test/UnitTest1.cs
@@ -5,13 +5,8 @@ namespace Compo.Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
-    }
-
     [Theory]
-    // [InlineData("@add(1,2)", 3)]
+    [InlineData("@add(1,2)", 3)]
     [InlineData("@abs(1.2)", 1)]
     [InlineData("@mult(2,2)", 4d)]
     [InlineData("@mult(2.1, 2.3)", 4.83)]
@@ -19,13 +14,11 @@ public class UnitTest1
     {
         var services = new ServiceCollection();
         services.AddLogging();
-        services.AddSingleton<FunctionRegistration>(_ => new FunctionRegistration
-            { FunctionType = typeof(MultiplicationFunction), FunctionName = "mult" });
 
         services.RegisterFunctions(typeof(MultiplicationFunction), "mult");
-        services.AddTransient<IFunction, MultiplicationFunction>();
-        services.AddTransient<MultiplicationFunction>();
+
         services.DiscoverFunctions();
+
         services.AddSingleton<IExpressionEvaluator, ExpressionEvaluator>();
 
         var serviceProvider = services.BuildServiceProvider();
@@ -34,12 +27,7 @@ public class UnitTest1
 
         var engine = new ExpressionParser();
         var actual = expressionEvaluator.Evaluate(engine.BuildAst(expression));
-        actual.GetType();
-        Assert.Equal(result, actual);
+
+        actual.Should().BeEquivalentTo(result);
     }
 }
-
-// E21 + E22
-
-
-
