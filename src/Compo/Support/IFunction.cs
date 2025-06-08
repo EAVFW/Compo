59cdaad214c8ca1f79876d0a34cfd76af3211021
Thyge Skødt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2025-06-08 13:12:22 +0200

    fix: Remove IConvertible type constraint on IFunction return values
    Functions should be able to return "objects" (Dictionaries), which does not fulfill the requirement

--- src/Compo/Support/IFunction.cs
+++ src/Compo/Support/IFunction.cs
@@ -4,14 +4,12 @@ public interface IFunction;
 
 public interface IFunction<in T, out TR> : IFunction
     where T : IConvertible
-    where TR : IConvertible
 {
     public TR Execute(T t);
 }
 
 public interface IFunctionParams<in T, out TR> : IFunction
     where T : IConvertible
-    where TR : IConvertible
 {
     public TR Execute(params T[] ts);
 }
@@ -19,7 +17,6 @@ public interface IFunctionParams<in T, out TR> : IFunction
 public interface IFunction<in T1, in T2, out TR> : IFunction
     where T1 : IConvertible
     where T2 : IConvertible
-    where TR : IConvertible
 {
     public TR Execute(T1 t1, T2 t2);
 }
@@ -28,7 +25,6 @@ public interface IFunction<in T1, in T2, in T3, out TR> : IFunction
     where T1 : IConvertible
     where T2 : IConvertible
     where T3 : IConvertible
-    where TR : IConvertible
 {
     public TR Execute(T1 t1, T2 t2, T3 t3);
 }
